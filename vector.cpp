2684a904cd9e2ba56a132c62df3b6856ae62f2b0
James Erick <erick.were@strathmore.edu>
2016-06-03 09:36:13 +0300

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+ #include <iostream>
+ int main()
+ {
+ using namespace std;
+ vector<int> vect;
+ for (int nCount=0; nCount < 6; nCount++)
+ // insert at end of array
+ vect.push_back(10 - nCount);
+ for (int nIndex=0; nIndex < vect.size(); nIndex++)
+ cout << vect[nIndex] << " ";
+ cout << endl;
+ }
